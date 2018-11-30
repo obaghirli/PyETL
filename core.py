5aaacd84723712509552cc52a486be41d2501984
orkhanbaghirli <orkhan.baghirli@metglobal.com>
2018-12-01 01:16:09 +0300

    PyETL: dropna added

--- core.py
+++ core.py
@@ -18,8 +18,11 @@ class Line(object):
     def __init__(self, raw_line, col_indices):
         self.raw_line = raw_line
         self.col_indices = col_indices
+
         self.parsed_raw_line = None
-        self.is_null = False
+        self.has_null = False
+
+        self.process_line()
 
     def parse(self, field_delimiter=";"):
         split_raw_line = self.raw_line.split(field_delimiter)
@@ -28,28 +31,33 @@ class Line(object):
         return self
 
     def check_null(self, null_char='-'):
-        self.is_null = True if null_char in self.parsed_raw_line else False
+        self.has_null = True if null_char in self.parsed_raw_line else False
         return self
 
+    def process_line(self):
+        self.parse(field_delimiter=";").check_null(null_char='-')
+
     def __repr__(self):
-        return "{0}:{1}".format(self.parsed_raw_line, self.is_null)
+        return "{0}:{1}".format(self.parsed_raw_line, self.has_null)
 
 
-class Transformers(object):
+class Transformer(object):
     def __init__(self):
-        pass
+        self.orig_data = None
+        self.transformed_data = None
 
+    def fit(self, data):
+        self.orig_data = data
+        return self
+
+    def dropna(self):
+        self.transformed_data = list(filter(lambda line_obj: line_obj.has_null is False, self.orig_data))
+        return self
+        
 
 class Utils(object):
     def __init__(self):
         pass
-
-    def map_columns(self, original_columns):
-        index_array = list()
-        for col in SPECIFIED_COLUMNS:
-            col_index = original_columns.index(col)
-            index_array.append(col_index)
-        return index_array
 
     def read_file(self, filename='Challenge_me.txt'):
         with open(filename) as fd:
@@ -64,14 +72,31 @@ class Utils(object):
                 return 'unnamed'
         return list(map(lambda x: header_udf(x), header_row.split(header_delimiter)))
 
+    def map_columns(self, original_columns):
+        index_array = list()
+        for col in SPECIFIED_COLUMNS:
+            col_index = original_columns.index(col)
+            index_array.append(col_index)
+        return index_array
+
 
 if __name__ == '__main__':
     sample_line = """14;std;sedan;3.31;20;9.00;3055;rwd;front;164;ohc;mpfi;gas;55.70;25;121,76;189.00;bmw;-;- ;four;4250;2456500;3.19;1;103.50;66.90"""
 
     utils = Utils()
+    transformer = Transformer()
+
     rows = utils.read_file(filename='Challenge_me.txt')
-    original_columns = utils.extract_headers(rows[0], header_delimiter=";")
+    header_row = rows[0]
+    data_rows = rows[1:]
+    original_columns = utils.extract_headers(header_row, header_delimiter=";")
     specified_columns_indices = utils.map_columns(original_columns)
-    line = Line(sample_line, specified_columns_indices)
-    print line.parse(field_delimiter=";").check_null(null_char="-")
+    # line = Line(sample_line, specified_columns_indices)
+    # print line.parse(field_delimiter=";").check_null(null_char="-")
+
+    line_objects = list(map(lambda data_row: Line(data_row, specified_columns_indices), data_rows))
+    transformer.fit(line_objects).dropna()
+
+    print len(line_objects)
+    print len(transformer.transformed_data)
 
